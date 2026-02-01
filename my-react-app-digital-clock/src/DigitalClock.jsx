9ffcb1ed34203835ed8466ec6daf4b062660d1de
william <espiw@farmingdale.edu>
2026-02-01 15:36:27 -0500

    useEffect functionality

--- my-react-app-digital-clock/src/DigitalClock.jsx
+++ my-react-app-digital-clock/src/DigitalClock.jsx
@@ -1,6 +1,25 @@
 import React, {useState, useEffect} from "react";
 
 function DigitalClock() {
+    // create state variable of time with setter
+    // initial state will be the current date/time
+    const [time, setTime] = useState(new Date());
+
+    // do this code only when we mount the component, not every time it renders
+    useEffect(() => {
+        // do some code every 1000ms, or every 1 second
+        const intervalId = setInterval(() => {
+            // update the state of time every second with the new current date/time
+            setTime(new Date());
+        }, 1000);
+
+        // clean up function when this component unmounts
+        return () => {
+            // if we unmount, this stops it from running continously
+            clearInterval(intervalId);
+        }
+    }, []);
+
     return(
         <div className="clock-container">
             <div className="clock">
