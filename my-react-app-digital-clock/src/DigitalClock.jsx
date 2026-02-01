1df08296caba055282052e479d63786af7249fac
william <espiw@farmingdale.edu>
2026-02-01 15:12:40 -0500

    digital clock html elements

--- my-react-app-digital-clock/src/DigitalClock.jsx
+++ my-react-app-digital-clock/src/DigitalClock.jsx
@@ -2,7 +2,11 @@ import React, {useState, useEffect} from "react";
 
 function DigitalClock() {
     return(
-        <p>hello</p>
+        <div className="clock-container">
+            <div className="clock">
+                <span>00:00:00</span>
+            </div>
+        </div>
     );
 }
 
